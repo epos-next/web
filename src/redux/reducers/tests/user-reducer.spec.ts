cce94dc4c451e6ea80e6d503f3502395d419e6e1
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-08-11 16:09:58 +0500

    cover user-reducer with unit tests

--- src/redux/reducers/tests/user-reducer.spec.ts
+++ src/redux/reducers/tests/user-reducer.spec.ts
@@ -1,7 +1,28 @@
+import userReducer, {initialState, setUser, State } from "@redux/reducers/user-reducer";
+import { User } from "../../../models/user";
 
 describe("Testing user reducer", () => {
 
-    it("2 + 2 should equal 4", () => {
-        expect(2 + 2).toEqual(4);
+    const initialsTestingState: State = {
+        user: {
+            name: "Yaroslav Zotov",
+            id: 1,
+        }
+    }
+
+    const user: User = {
+        name: "Mike Wazowski",
+        id: 2,
+    }
+
+    it("should handle initial state", () => {
+        const state = userReducer(undefined, { type: "test" });
+        expect(state).toEqual(initialState);
+    })
+
+    it("should handle setUser", () => {
+        const state = userReducer(initialsTestingState, setUser(user));
+        expect(state.user?.name).toEqual("Mike Wazowski")
+        expect(state.user?.id).toEqual(2)
     })
 })
