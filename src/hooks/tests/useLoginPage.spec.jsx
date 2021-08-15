0483a9b70979bb00995f889077d4eeb394050ffb
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-08-16 01:55:30 +0500

    test(useLoginPage): made run using AppRouter

--- src/hooks/tests/useLoginPage.spec.jsx
+++ src/hooks/tests/useLoginPage.spec.jsx
@@ -5,8 +5,7 @@ import AuthHelper from "../../helpers/auth-helper";
 import UiHelper from "../../helpers/ui-helper";
 import ApiService from "../../services/api-service";
 import {tokensBody} from "../../../test/fixtures";
-import {createHistory, createMemorySource, LocationProvider} from "@reach/router";
-import {navigate} from "gatsby";
+import AppRouter from "../app-router";
 
 it("should be idle (not loading) by default", () => {
     const {result} = renderHook(useLoginPage);
@@ -17,9 +16,8 @@ it("should handle email and password", async () => {
     const {result} = renderHook(() => useLoginPage());
     jest.spyOn(ApiService, "authenticate").mockResolvedValueOnce(tokensBody)
     jest.spyOn(AuthHelper, "tokens", "set")
-    jest.mock('@reach/router', () => ({
-        navigate: jest.fn(),
-    }))
+    const routerHomePrev = AppRouter.goHome;
+    const routerMock = AppRouter.goHome = jest.fn()
     jest.spyOn(UiHelper, "showErrorToast")
 
     const email = "test@zotov.dev"
@@ -36,6 +34,7 @@ it("should handle email and password", async () => {
     }))
 
     expect(ApiService.authenticate).toBeCalledWith(email, password)
+    AppRouter.goHome = routerHomePrev
     jest.clearAllMocks()
 });
 
@@ -43,8 +42,9 @@ describe("testing onSubmit()", () => {
 
     it("should login", async () => {
         const {result} = renderHook(() => useLoginPage());
-        window.___navigate = () => {}
         jest.spyOn(ApiService, "authenticate").mockResolvedValueOnce(tokensBody)
+        const routerHomePrev = AppRouter.goHome;
+        const routerMock = AppRouter.goHome = jest.fn()
         const authMock = jest.spyOn(AuthHelper, "tokens", "set")
 
         const email = "test@zotov.dev"
@@ -60,6 +60,9 @@ describe("testing onSubmit()", () => {
             }
         }))
         expect(authMock).toBeCalledWith(tokensBody)
+        expect(routerMock).toBeCalledTimes(1)
+        jest.clearAllMocks();
+        AppRouter.goHome = routerHomePrev;
     });
 
     it("should handle invalid credentials", async () => {
@@ -82,6 +85,29 @@ describe("testing onSubmit()", () => {
 
         expect(UiHelper.showErrorToast).toBeCalledWith(
             "Неверный Email или пароль. Проверьте корректность введённых данных."
+        )
+    });
+
+    it("should handle server error", async () => {
+        const {result} = renderHook(() => useLoginPage());
+        jest.spyOn(ApiService, "authenticate").mockReturnValue("server-error")
+        jest.spyOn(UiHelper, "showErrorToast").mockImplementation()
+
+        const email = "test@zotov.dev"
+        const password = "12345678aSd"
+
+        act(() => {
+            result.current.handlers.onEmailChanged({target: {value: email}})
+            result.current.handlers.onPasswordChanged({target: {value: password}})
+        })
+
+        await act(() => result.current.handlers.onSubmit({
+            preventDefault() {
+            }
+        }))
+
+        expect(UiHelper.showErrorToast).toBeCalledWith(
+            "Похоже что наши сервисы сейчас недоступны. Повторите попытку чуть позже"
         )
     });
 
