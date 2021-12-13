a3993e5990065894def776ae2b1a89b2e718bfd3
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-12-13 19:57:50 +0500

    ci: testing. trigger build

--- src/utils/api/routes.ts
+++ src/utils/api/routes.ts
@@ -14,6 +14,7 @@ export default class ApiRoutes {
     );
     static createControlWork = ApiRoutes.buildRoute("/control-work")
     static createAd = ApiRoutes.buildRoute("/advertisement")
+
     static setHomeworkStatus = (id: number, status: boolean) => ApiRoutes.buildRoute(
         `/homework/${ id }/${ status ? "complete" : "cancel-complete" }`
     )
