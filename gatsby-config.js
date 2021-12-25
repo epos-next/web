e1de33505dfed1fb92ed61985441d46190726d3c
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-12-25 20:31:11 +0500

    feat: remove gatsby-plugin-manifest
    somehow its break all app

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,17 +13,5 @@ module.exports = {
         "gatsby-plugin-typescript",
         `gatsby-plugin-sass`,
         "gatsby-plugin-use-query-params",
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Эпос Next`,
-                short_name: `Эпос`,
-                start_url: `/`,
-                background_color: `#FFF`,
-                theme_color: `#9569FD`,
-                display: "standalone",
-                icon: `static/favicon.png`,
-            },
-        }
     ],
 };
